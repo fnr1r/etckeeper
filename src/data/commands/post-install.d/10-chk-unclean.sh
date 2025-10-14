bea5f9ad4845d9dce304073a6e53de7519bfa3c7
fnrir <fnr1r0@protonmail.com>
2025-10-14 07:01:22 +0200

    fix: logic error in post-install

--- src/data/commands/post-install.d/10-chk-unclean.sh
+++ src/data/commands/post-install.d/10-chk-unclean.sh
@@ -3,7 +3,7 @@
 pre_install="$ETCKEEPER_CACHE_DIR/packagelist.pre-install"
 
 # TODO: Is this supposed to exit with 0 on clean?
-if ! "$0" unclean; then
+if "$0" unclean; then
     _do_cleanup
     exit 0
 fi
